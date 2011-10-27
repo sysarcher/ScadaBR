0a5427ef5f8239706ec422649035b265f0bd07ac
arnep <arnep@2931184b-f66e-4952-b696-a5c77993f6f7>
2011-10-27 13:27:34 +0000

    moved trunk/scadabr to branches/scadabr_1_0_0/scadabr

--- trunk/scadabr/scadabr/src/test/resources/db/setUp-Table-Users.sql
+++
@@ -1,6 +0,0 @@
-insert into users (id, mangoUsername, mangoUserPassword, email, phone, mangoAdmin, disabled, receiveAlarmEmails, receiveOwnAuditEvents)
-values (-10, 'User10', 'UserPassword10', '10@user', '0000010', true, true, 'NONE', false);
-insert into users (id, mangoUsername, mangoUserPassword, email, phone, mangoAdmin, disabled, receiveAlarmEmails, receiveOwnAuditEvents)
-values (-11, 'User11', 'UserPassword11', '11@user', '0000011', true, true, 'NONE', false);
-insert into users (id, mangoUsername, mangoUserPassword, email, mangoAdmin, disabled, receiveAlarmEmails, receiveOwnAuditEvents)
-values (-12, 'User12', 'UserPassword12', '12@user', false, false, 'CRITICAL', false);
