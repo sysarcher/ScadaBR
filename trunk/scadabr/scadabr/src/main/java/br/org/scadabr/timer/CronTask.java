0eb5af570580255f2897812bd15444a1337e745f
arnep <arnep@2931184b-f66e-4952-b696-a5c77993f6f7>
2014-04-01 14:51:24 +0000

    one was missing

---
+++ trunk/scadabr/scadabr/src/main/java/br/org/scadabr/timer/CronTask.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.org.scadabr.timer;
+
+import br.org.scadabr.timer.cron.CronExpression;
+import br.org.scadabr.timer.cron.CronParser;
+import java.text.ParseException;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author aploese
+ */
+public abstract class CronTask implements Runnable {
+
+    final Object lock = new Object();
+
+    int state = VIRGIN;
+
+    static final int VIRGIN = 0;
+
+    static final int SCHEDULED   = 1;
+
+    static final int EXECUTED    = 2;
+
+    static final int CANCELLED   = 3;
+    
+    CronExpression cronExpression;
+
+    long nextExecutionTime;
+    
+    protected long currentTimeInMillis;
+
+    protected CronTask(String pattern) throws ParseException {
+        CronParser cp = new CronParser();
+        cronExpression = cp.parse(pattern);
+    }
+
+    public boolean cancel() {
+        synchronized(lock) {
+            boolean result = (state == SCHEDULED);
+            state = CANCELLED;
+            return result;
+        }
+    }
+
+    public long calcNextExecutionTimeAfter(GregorianCalendar c) {
+        synchronized(lock) {
+            cronExpression.calcNextValidTimeAfter();
+            cronExpression.setNextTimestampTo(c);
+            nextExecutionTime = c.getTimeInMillis();
+            return nextExecutionTime;
+        }
+        
+    }
+    
+    public long getNextScheduledExecutionTime() {
+        synchronized(lock) {
+            return nextExecutionTime;
+        }
+    }
+}
