dc79ae4fcaf8b9ba8145781fbafa563e76d15e1d
arnep <arnep@2931184b-f66e-4952-b696-a5c77993f6f7>
2014-06-17 19:59:51 +0000

    moved out java script code of pages to separate modules(files)

--- scadabr/scadabr/src/main/webapp/WEB-INF/jsp/pointHierarchy.jsp
+++ scadabr/scadabr/src/main/webapp/WEB-INF/jsp/pointHierarchy.jsp
@@ -20,40 +20,12 @@
 <%@page import="com.serotonin.mango.Common"%>
 <tag:page >
     <script type="text/javascript">
-        var selectedFolderNode;
-        var tree;
-        var myRestStore;
-
-        window.onload = function() {
-
-            require([
-                "dojo/store/JsonRest",
-                "dijit/Tree",
-                "dojo/domReady!"
-            ], function(JsonRest, Tree) {
-
-                myRestStore = new JsonRest({
-                    target: "dstree/",
-                    getChildren: function(object, onComplete, onError) {
-                        this.query({parentId: object.id}).then(onComplete, onError);
-                    },
-                    mayHaveChildren: function(object) {
-                        return object.nodeType === "PF";
-                    },
-                    getRoot: function(onItem, onError) {
-                        this.get("root").then(onItem, onError);
-                    },
-                    getLabel: function(object) {
-                        return object.name;
-                    }
-                });
-                // Create the Tree.
-                tree = new Tree({
-                    model: myRestStore
-                }, "treeDiv");
-                tree.startup();
-            });
-        }
+        require([
+            "scadabr/jsp/PointHierarchy",
+            "dojo/domReady!"
+        ], function(PointHierarchy) {
+            var pointHierarchy = new PointHierarchy("treeDiv");
+        });
 
     </script>
 
---
+++ scadabr/scadabr/src/main/webapp/resources/scadabr/jsp/WatchList.js
@@ -0,0 +1,89 @@
+define(["dojo/_base/declare",
+    "dojo/_base/lang",
+    "dijit/Tree",
+    "dojo/store/JsonRest",
+    "dgrid/OnDemandGrid",
+    "dgrid/Keyboard",
+    "dgrid/Selection",
+    "dojo/request",
+    "dojo/store/Memory"
+], function(declare, lang, Tree, JsonRest, OnDemandGrid, Keyboard, Selection, request, Memory) {
+
+    return declare(null, {
+        pointsTreeStore: null,
+        pointsTree: null,
+        watchlistGrid: null,
+        constructor: function(pointsTreeNode, watchListNode, watchlistId) {
+            this.initPointsTree(pointsTreeNode);
+            this.initWatchListTable(watchListNode, watchlistId);
+        },
+        initPointsTree: function(pointsTreeNode) {
+            this.pointsTreeStore = new JsonRest({
+                target: "dstree/",
+                getChildren: function(object, onComplete, onError) {
+                    this.query({parentId: object.id}).then(onComplete, onError);
+                },
+                mayHaveChildren: function(object) {
+                    return object.nodeType === "PF";
+                },
+                getRoot: function(onItem, onError) {
+                    this.get("root").then(onItem, onError);
+                },
+                getLabel: function(object) {
+                    return object.name;
+                }
+            });
+            // Create the Tree.
+            this.pointsTree = new Tree({
+                model: this.pointsTreeStore
+            }, pointsTreeNode);
+        },
+        initWatchListTable: function(watchListNode, watchlistId) {
+            this.watchlistGrid = new (declare([OnDemandGrid, Keyboard, Selection]))({
+                showHeader: false,
+                store: new Memory({data: null}),
+                columns: {
+                    chartType: {
+                        label: "ChartType"
+                    },
+                    id: {
+                        label: "Id"
+                    },
+                    settable: {
+                        label: "Settable"
+                    },
+                    canonicalName: {
+                        label: "Name"
+                    },
+                    timestamp: {
+                        lable: "Timestamp"
+                    },
+                    value: {
+                        lable: "Value"
+                    }
+                },
+                loadingMessage: "Loading data...",
+                noDataMessage: "No results found.",
+                selectionMode: "single" // for Selection; only select a single row at a time
+                        //cellNavigation: false, // for Keyboard; allow only row-level keyboard navigation
+            }, watchListNode);
+            //get initial list...
+            this.fetchWatchList(watchlistId);
+        },
+        fetchWatchList: function(watchlistId) {
+            request("rest/watchlists/", {
+                query: {id: watchlistId},
+                handleAs: "json"
+            }).then(lang.hitch(this, function(data) {
+                console.log("WL DATA: " + data);
+                this.watchlistGrid.store.setData(data.points);
+                this.watchlistGrid.refresh();
+            }), function(err) {
+                console.log("WL ERR: " + err);
+            }, function(evt) {
+                console.log("WL EVT: " + evt);
+            });
+        }
+
+    });
+});
