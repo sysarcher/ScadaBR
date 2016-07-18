5998e82d2ed1023eb90f26abc0ecb0354ea5740f
arnep <arnep@2931184b-f66e-4952-b696-a5c77993f6f7>
2016-07-18 20:32:10 +0000

    dataPoints should be working as SPA demo (browser back/forward button...)

--- trunk/scadabr/scadabr/src/main/webapp/resources/scadabr/desktop/Header.js
+++ trunk/scadabr/scadabr/src/main/webapp/resources/scadabr/desktop/Header.js
@@ -8,7 +8,7 @@ define([
     ContentPane
 ) {
     return declare("scadabr/desktop/Header", [ContentPane, _TemplatedMixin], {
-        templateString: '<div><b><i>ScadaBR @' + window.location + '</i></b><img src="images/mangoLogoMed.jpg" alt="Logo" style="float:right;"/><div>',
+        templateString: '<div><b><i>ScadaBR @' + window.location.origin + window.location.pathname + '</i></b><img src="images/mangoLogoMed.jpg" alt="Logo" style="float:right;"/><div>',
         
         postCreate: function () {
             this.inherited(arguments);
