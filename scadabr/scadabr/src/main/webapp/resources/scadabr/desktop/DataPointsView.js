44a86e46b58e5f414e509c4001592c9a629aef32
arnep <arnep@2931184b-f66e-4952-b696-a5c77993f6f7>
2016-07-20 18:39:22 +0000

    start edit datapoints ...

--- scadabr/scadabr/src/main/webapp/resources/scadabr/desktop/DataPointsView.js
+++ scadabr/scadabr/src/main/webapp/resources/scadabr/desktop/DataPointsView.js
@@ -4,13 +4,15 @@ define(["dojo/_base/declare",
     "dijit/layout/BorderContainer",
     "dijit/layout/ContentPane",
     "dijit/layout/TabContainer",
+    "scadabr/desktop/DataPointProperties",
     "dojo/i18n!scadabr/desktop/nls/messages"
-], function (declare, TreeModel, NavigationTree, BorderContainer, ContentPane, TabContainer, messages) {
+], function (declare, TreeModel, NavigationTree, BorderContainer, ContentPane, TabContainer, DataPointProperties, messages) {
 
     return declare("scadabr/desktop/DataPointsView", [BorderContainer], {
         gutters: true,
         liveSplitters: true,
         tree: null,
+        treeContentPane: null,
         detailView: null,
         model: null,
         baseHref: null,
@@ -19,15 +21,30 @@ define(["dojo/_base/declare",
         postCreate: function () {
             this.inherited(arguments);
 
-            this.model = new TreeModel("REST/");
+            this.model = new TreeModel(this.restBaseUrl);
             this.tree = new NavigationTree({model: this.model, baseHref: this.baseHref});
-            this.tree.startup();
-            this.addChild(this.tree);
+//            this.tree.startup();
+            this.treeContentPane = new ContentPane({region: 'left',
+                splitter: 'true'});
+            this.treeContentPane.addChild(this.tree);
+            this.addChild(this.treeContentPane);
 
             this._initDetailViewModel();
         },
         setCurrentId: function (id) {
             this.tree.selectNode(id);
+            var self = this;
+            this.tree.model.refresh(id).then(function (object) {
+                if (object.nodeType.startsWith("DATA_POINT")) {
+                    self.addChild(self.detailView);
+                    self.dataPointProperties.setDataPoint(object);
+                } else {
+                    self.removeChild(self.detailView);
+
+                }
+            }, function (error) {
+                self.removeChild(self.detailView);
+            });
             //TODO set DetailView
         },
         _initDetailViewModel: function () {
@@ -38,6 +55,9 @@ define(["dojo/_base/declare",
             this.detailView.addChild(this.detailView.tableDataView);
             this.detailView.editDataView = new ContentPane({title: "Edit"});
             this.detailView.addChild(this.detailView.editDataView);
+            //QUick and dirty!!!
+            this.dataPointProperties = new DataPointProperties({});
+            this.detailView.editDataView.addChild(this.dataPointProperties);
             this.detailView.eventsDataView = new ContentPane({title: "Point Events and Notes"});
             this.detailView.addChild(this.detailView.eventsDataView);
             this.detailView.usageDataView = new ContentPane({title: "Point Usage"});
